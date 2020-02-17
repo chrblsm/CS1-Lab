0f6feb00e30c0b801b70638aa24100f9453a1663
bdalila27 <60198942+bdalila27@users.noreply.github.com>
2020-02-17 15:31:58 -0600

    Add files via upload

---
+++ lab 17Feb.py
@@ -0,0 +1,33 @@
+#1
+for number in range (1,26):
+  print(number**2)
+
+#2
+y=()
+while(y!="haha"):
+  y=input("Write 'It's a Loop:  ")
+
+#3
+positive=float(input())
+while(positive>0):
+  positive-=0.5
+  print(positive)
+
+#4
+a=()
+while(a!="no"):
+  a=input("Do you want to continue?  ")
+print("This is the end")
+
+#Level 1 Loops
+
+#1
+for i in range(3):
+  print(i)
+
+#4
+list1=[1,2,3,4,5]
+for element in list1:
+  print(element**2)
+
+
